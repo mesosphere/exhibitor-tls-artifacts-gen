3dadd006fcb8d0d20baa2a37c49f7062e9d9d5e3
Jared Rodriguez <jared@blacknode.net>
2019-05-23 10:00:56 -0500

    Address PR comments

--- tests/test_gen_artifacts.py
+++ tests/test_gen_artifacts.py
@@ -1,26 +1,22 @@
 import stat
+import textwrap
 from pathlib import Path
 
 import click
 import click.testing
 
-
 from exhibitor_tls_artifacts.gen_artifacts import app
 
 
 class TestCLI:
+
     @staticmethod
     def _validate_files(directory):
         """ Ensures that artifacts are created and that they have the
         appropriate permission """
-        files = [
-            ('client-cert.pem', 0o100644),
-            ('client-key.pem', 0o100600),
-            ('clientstore.jks', 0o100600),
-            ('root-cert.pem', 0o100644),
-            ('serverstore.jks', 0o100600),
-            ('truststore.jks', 0o100600)
-        ]
+        files = [('client-cert.pem', 0o100644), ('client-key.pem', 0o100600),
+                 ('clientstore.jks', 0o100600), ('root-cert.pem', 0o100644),
+                 ('serverstore.jks', 0o100600), ('truststore.jks', 0o100600)]
         for f, mode in files:
             full_path = directory / f
             assert full_path.exists()
@@ -32,8 +28,9 @@ class TestCLI:
 
         with runner.isolated_filesystem() as temp:
             temp_path = Path(temp)
-            result = runner.invoke(
-                app, args=['10.10.10.10'], catch_exceptions=False)
+            result = runner.invoke(app,
+                                   args=['10.10.10.10'],
+                                   catch_exceptions=False)
 
             assert result.exit_code == 0
             artifact_path = temp_path / 'artifacts' / 'node_10_10_10_10'
@@ -44,8 +41,9 @@ class TestCLI:
         """ Test outputting artifacts to a customer location """
         runner = click.testing.CliRunner()
         new_path = tmp_path / 'new'
-        result = runner.invoke(
-            app, args=['-d', new_path, '10.10.10.10'], catch_exceptions=False)
+        result = runner.invoke(app,
+                               args=['-d', new_path, '10.10.10.10'],
+                               catch_exceptions=False)
 
         assert result.exit_code == 0
         self._validate_files(new_path / 'node_10_10_10_10')
@@ -55,29 +53,49 @@ class TestCLI:
         runner = click.testing.CliRunner()
 
         with runner.isolated_filesystem() as temp:
-            err = "Artifacts directory cannot exist!"
+            err = textwrap.dedent("""\
+            Usage: exhibitor-tls-artifacts [OPTIONS] [NODES]...
+
+            Error: Invalid value for "-d" / "--output-directory": Artifacts directory cannot exist!
+            """)
             temp_path = Path(temp)
             output_dir = temp_path / 'exists'
             output_dir.mkdir()
-            result = runner.invoke(
-                app, args=['-d', output_dir, '10.10.10.10'], catch_exceptions=False)
+            result = runner.invoke(app,
+                                   args=['-d', output_dir, '10.10.10.10'],
+                                   catch_exceptions=False)
             assert result.exit_code == 2
-            assert err in result.stdout
+            assert err == result.stdout
 
     def test_no_nodes(self):
         """ Test error case if no arguments are provided """
-        err = "No nodes have been provided."
+        err = textwrap.dedent("""\
+        Usage: exhibitor-tls-artifacts [OPTIONS] [NODES]...
+
+        Error: Invalid value for "-d" / "--output-directory": Artifacts directory cannot exist!
+        """)
         runner = click.testing.CliRunner()
         result = runner.invoke(app, catch_exceptions=False)
         assert result.exit_code == 2
+        print(result.stdout)
         assert err in result.stdout
 
     def test_help(self):
         """ Tests if there are any changes to the help output. This
         is only useful for coercing a developer to write CLI tests if
         an option/argument is added. """
-        help_bytes = 435
+        expected_help = textwrap.dedent("""\
+            Usage: exhibitor-tls-artifacts [OPTIONS] [NODES]...
+
+              Generates Admin Router and Exhibitor TLS artifacts. NODES should consist of
+              a space separated list of master ip addresses. See
+              https://docs.mesosphere.com/1.13/security/ent/tls-ssl/exhibitor-tls/
+
+            Options:
+              -d, --output-directory TEXT  Directory to put artifacts in. This
+                                           output_directory must not exist.
+              --help                       Show this message and exit.
+            """)
         runner = click.testing.CliRunner()
         result = runner.invoke(app, args=['--help'])
-        assert len(result.stdout_bytes) == help_bytes
-
+        assert expected_help == result.stdout
